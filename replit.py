98384f8846bb07111014f50e3de2134310556b2d
Aidan Salzmann <aidansalzmann@Aidans-MacBook-Pro.local>
2022-07-01 18:36:33 -0400

    add ibc tokens

--- replit.py
+++ replit.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 
 
-
 client = discord.Client()
 WL = ["stride19uvw0azm9u0k6vqe4e22cga6kteskdqq3ulj6q"]
 
@@ -31,6 +30,9 @@ def rateLimit(address):
         if diff < 86397:
             return True
 
+ustrd_denom = "ustrd"
+ibc_denom = "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -44,11 +46,18 @@ async def on_message(message):
             print(e)
             await message.channel.send("Error: you must format your message as $balance stride19uvw0azm9u0k6vqe4e22cga6kteskdqq3ulj6q, using your address")
             return
-        d = requests.get(f"http://stride-node3.internal.stridenet.co:1317/cosmos/bank/v1beta1/balances/{addr}/by_denom?denom=ustrd").json()
+        # STRD BALANCES
+        d = requests.get(f"https://stride-node3.internal.stridenet.co:443/cosmos/bank/v1beta1/balances/{addr}/by_denom?denom={ustrd_denom}").json()
         micro = d['balance']['amount']
         strd = microDenomToDenom(micro)
         msg = f"balance for: {addr} is {strd} STRD"
-        await message.channel.send(msg)
+
+        # IBC BALANCES
+        d2 = requests.get(f"https://stride-node3.internal.stridenet.co:443/cosmos/bank/v1beta1/balances/{addr}/by_denom?denom={ibc_denom}").json()
+        ibc_micro = d2['balance']['amount']
+        ibc = microDenomToDenom(ibc_micro)
+        msg2 = f"\nbalance for: {addr} is {ibc} ATOM on Stride"
+        await message.channel.send(msg + msg2)
 
     elif message.content.startswith('$faucet'):
         try:
@@ -64,15 +73,16 @@ async def on_message(message):
             await message.channel.send(f'Please wait 24 hours to use the faucet')
             return
         else:
-            # curl -X POST -d '{"address": "stride19uvw0azm9u0k6vqe4e22cga6kteskdqq3ulj6q"}' stride-node2.internal.stridenet.co:8000
-            url = 'http://stride-node2.internal.stridenet.co:8000'
+            # curl -X POST -d '{"address": "stride19uvw0azm9u0k6vqe4e22cga6kteskdqq3ulj6q"}' stride-node2.internal.stridenet.co:7936
+            url = 'http://stride-node2.internal.stridenet.co:27936'
             myobj = {'address': f'{addr}'}
             x = requests.post(url, json = myobj)
             msg = x.status_code
-            if msg == "200":
-                await message.channel.send(f'Sent 10 STRD to {addr}, {msg}')
+            if msg == 200:
+                await message.channel.send(f'Sent 10 STRD and 10 ATOM to {addr}, {msg}')
             else:
-                await message.channel.send(f'Failed to send STRD, faucet is down!')
+                print(x)
+                await message.channel.send(f'Failed to send STRD, the faucet might be down or you\'ve hit your faucet limit.')
     else:
         await message.channel.send(f'Query your balance or request tokens, every 24 hours. Commands are \n\t $balance <address> \n\t $faucet <address>')
 
